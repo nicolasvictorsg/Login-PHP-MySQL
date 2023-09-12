33a22365a8f40371dbf31aa1704f9ef145b20ffb
Nicolas Victor <nicolas.victor2303@gmail.com>
2023-09-12 15:51:29 -0300

    Páginas

---
+++ Cadastro/painel.php
@@ -0,0 +1,18 @@
+<?php
+    if(!isset($_SESSION)){
+        session_start();
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+</head>
+<body>
+    Bem vindo ao Painel. <?php echo $_SESSION['nome'];?>
+</body>
+</html>
